0ccb5c6ad6ca0a058cb9aae150c09e1fd603bd21
M.Halil ERİŞEN <mutluhalilerisen@hotmail.com>
2021-07-26 20:13:59 +0300

    Asal Çarpanları Bulan Konsol Uygulaması Güncellendi.

--- prime_factor.dart
+++ prime_factor.dart
@@ -1,18 +1,40 @@
 import 'dart:io';
 
-Set<int> listOfPrimeFactor(int number) {
-  Set<int> listOfPrime = {};
+List<int> listOfPrimeFactor(int number) {
+  List<int> listOfPrimeFactors = [];
+  Set<int> listOfPrime = generatePrimeNumbers(number ~/ 2);
   int numberCopy = number;
 
-  for (int i = 2; i <= number / 2; i++) {
-    if (numberCopy % i == 0) {
-      listOfPrime.add(i);
-      numberCopy = numberCopy ~/ i;
-
+  for (int i=0; i < listOfPrime.length; i++) {
+    if (numberCopy % listOfPrime.elementAt(i) == 0) {
+      listOfPrimeFactors.add(listOfPrime.elementAt(i));
+      numberCopy = numberCopy ~/ listOfPrime.elementAt(i);
       i--;
     }
+    
   }
-  return listOfPrime;
+
+  return listOfPrimeFactors;
+}
+
+//Verilen Sayıya Kadar Asal Sayı Üreten Fonksiyon
+Set<int> generatePrimeNumbers(int number) {
+  Set<int> primeNumbers = {};
+  for (int i = 2; i < number; i++) {
+    //Her sayı Varsayılan Olarak Asal
+    bool isPrime = true;
+    //Bir sayının kendisinden sonra en büyük böleni yarısıdır.
+    for (int k = 2; k < i; k++) {
+      //Eğer Sayı Herhangi bir sayıya bölünürse Asal Değildir.
+      if (i % k == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) primeNumbers.add(i);
+  }
+  return primeNumbers;
 }
 
 main(List<String> args) {
